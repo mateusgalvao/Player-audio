c8e395dea1d24f5bc0aa0f85b6f12178b28a34e6
mateusgalvao <mateusgalvao908@gmail.com>
2021-05-03 10:54:49 -0300

    Playe audio nivel Hard

---
+++ hard/data.js
@@ -0,0 +1,21 @@
+export default [
+{
+        title:
+            "Slipknot - Vermilion",
+        artist: "Slipknot",
+        cover: "alt.jpg",
+        file: "aud.mp3"
+    },
+    {
+        title: "THE ROCK feat. EMINEM - DWAYNE JOHNSON MOTIVATION",
+        artist: "THE ROCK",
+        cover: "op.jpg",
+        file: "the.mp3"
+    },
+    {
+        title: "xxxxx-xxxxxxx-xxxxxx?",
+        artist: "Diego Fernandes",
+        cover: "alt.jpg",
+        file: "of.mp3"
+    },
+];
